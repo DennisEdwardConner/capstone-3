f9662fdac5a5102f11a6d0c7767032b691d35862
RacheleRice <104693615+RacheleRice@users.noreply.github.com>
2022-11-16 13:38:48 -0800

    reverting brewery controller back to og

--- java/src/main/java/com/techelevator/controller/BreweryController.java
+++ java/src/main/java/com/techelevator/controller/BreweryController.java
@@ -9,39 +9,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-    @RestController
-    @CrossOrigin
-//    @PreAuthorize("isAuthenticated()")
+@RestController
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class BreweryController {
 
-    public class BreweryController {
+    private BreweryDao breweryDao;
 
-        private BreweryDao breweryDao;
+    public BreweryController(BreweryDao breweryDao) {this.breweryDao = breweryDao;}
 
-        public BreweryController(BreweryDao breweryDao) {this.breweryDao = breweryDao;}
+    @GetMapping(path="/brewery/all")
+    public List<Brewery> getAllBreweries() { return breweryDao.findAll(); }
 
-        @RequestMapping(path="/breweries", method = RequestMethod.GET)
-        public List<Brewery> getAllBreweries() { return breweryDao.findAll(); }
+    @GetMapping(path="/brewery/{id}")
+    public Brewery getBreweryById(@PathVariable int id) { return breweryDao.getBreweryById(id); }
 
-        @RequestMapping(path="/brewery/{id}", method = RequestMethod.GET)
-        public Brewery getBreweryById(@PathVariable ("id") Integer id) {
-            return breweryDao.getBreweryById(id); }
-
-        @RequestMapping(path="/brewery/{name}", method = RequestMethod.GET)
-        public Brewery findBreweryByName(@PathVariable ("name") String name) {
-            return breweryDao.findByBreweryName(name);
-        }
-
-        @RequestMapping(path= "/brewery/create", method = RequestMethod.POST)
-//        @PostMapping(path="/brewery/create")
-        public boolean createBrewery(@RequestBody Brewery brewery) {
-            return breweryDao.createBrewery(brewery);
-        }
-
-        @RequestMapping(path="/brewery/{id}", method = RequestMethod.PUT)
-        public boolean updateBrewery(@RequestBody Brewery brewery, @PathVariable ("id") Integer id) {
-            return breweryDao.updateBrewery(brewery);
-        }
-
+    @GetMapping(path="/brewery/q={name}")
+    public Brewery findBreweryByName(@PathVariable String name) {
+        return breweryDao.findByBreweryName(name);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(path="/brewery/create")
+    public boolean createBrewery(@RequestBody Brewery brewery) {
+        return breweryDao.createBrewery(brewery);
+    }
 
+    @PutMapping(path="/brewery/{id}")
+    public boolean updateBrewery(@RequestBody Brewery brewery, @PathVariable int id) {
+        return breweryDao.updateBrewery(brewery);
+    }
+
+}
