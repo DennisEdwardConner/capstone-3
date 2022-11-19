790e0b3be107ca26ee8b7eff3d1c1846e3982330
RacheleRice <104693615+RacheleRice@users.noreply.github.com>
2022-11-18 22:35:17 -0800

    building brewery card for home page

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -5,9 +5,19 @@ import axios from "axios";
 import { GlobalContext } from "../../context/globalContext";
 import { useEffect } from "react";
 
+
+const Card= ({img,title,id,description})=>{
+    return <div style={{width:'400px',display:'flex',flexDirection:'column'}}>
+        <img style={{width:"400px",height:"300px"}} src={img} alt={id}/>
+        <h5 >{title}</h5>
+         <p >{description}</p>
+    </div>
+}
+
 const Home = ()=>{
 //brewery list 
     const [breweries,setBreweries]=useState([])
+    const [brewery,setBrewery]=useState({})
 //token from the context
     const {token} = useContext(GlobalContext)
  //passing token as header inside the getbrewery/all request and got the data
@@ -21,16 +31,18 @@ const Home = ()=>{
         // res.then((data)=> {console.log(data);}).catch((err)=>{console.log(err,'===err');})
 //set the data to brewery list
         setBreweries(res.data)
+        console.log(breweries,'=res====');
     }
 
-    // console.log(breweries,'=res====');
+    
 
     return <>
     <h2>I am from home component </h2>
     <button onClick={getBreweries}>getBreweries</button>
     <div><h4>Select Brewery</h4>
-        {breweries?.length!==0?<select>{breweries.map((item)=><option key={item.breweryId}>{item.breweryName}</option>)}</select>:""}
+        {breweries?.length!==0?<select>{breweries.map((item)=>{ return(<option onClick={()=>{setBrewery(item)}} key={item.breweryId}>{item.breweryName}</option>)})}</select>:""}
     </div>
+    <div><Card img={brewery?.breweryImg} title={brewery?.breweryName} description={brewery?.description} id={brewery?.breweryId} /></div>
     </>
 }
 
